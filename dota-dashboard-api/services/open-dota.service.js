3882b60addc4e601415f170039de90366c899c09
Leighton Ho <l.ho@hotmail.co.nz>
2019-10-02 23:14:15 +1300

    Enriching match data in the open-dota service.

--- dota-dashboard-api/services/open-dota.service.js
+++ dota-dashboard-api/services/open-dota.service.js
@@ -1,32 +1,52 @@
-const rp = require('request-promise');
+const request = require('request-promise');
 
 const getMatchHistory = async (accounts) => {
     console.log('service getMatchHistory')
-    let matchHistory = []
+    let matchHistory = {}
 
     for (const accountID of accounts.split(',')) {
         // fetch the recent match history for each account
         console.log(`get match history for ${accountID}`)
 
         await getMatchHistoryByAccountID(accountID).then(result => {
-            console.log(`appending ${result}`)
-            matchHistory.push(JSON.parse(result))
+            matchHistory[accountID] = result
         })
     }
 
     console.log(`there are ${matchHistory.length} result(s)`)
 
-    return matchHistory
+    let result = processMatchHistory(matchHistory)
+
+    return result
 }
 
 const getMatchHistoryByAccountID = async (accountID) => {
     const recentMatchesUrl = `https://api.opendota.com/api/players/${accountID}/matches?limit=10&api_key=YOUR-API-KEY`
     console.log(`service getMatchHistoryByAccountID ${recentMatchesUrl}`)
-    let result = ''
+    let result = null
 
-    await rp(recentMatchesUrl).then(response => {
-        result += response
+    await request(recentMatchesUrl).then(response => {
+        result = JSON.parse(response)
     })
+
+    return result
+}
+
+const processMatchHistory = (matchHistory) => {
+    console.log('processing the result set')
+    let result = []
+
+    for (let account_id in matchHistory) {
+        for (match of matchHistory[account_id]) {
+            let start_time_utc = 
+            match.start_time_utc = new Date(match.start_time * 1000).toUTCString()
+            match.account_id = account_id
+            result.push(match)
+        }
+    }
+
+    // order the results by game start time descending
+    result = result.sort((a, b) => b.start_time - a.start_time)
 
     return result
 }
