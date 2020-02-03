26305c8e591073a1345da8dc924f6b2e5b9cfc95
Leighton Ho <l.ho@hotmail.co.nz>
2020-02-03 22:33:50 +1300

    Fix issue where matches with Void Spirit won't load.

--- dota-dashboard/src/components/MatchDetail.js
+++ dota-dashboard/src/components/MatchDetail.js
@@ -5,6 +5,26 @@ import "../styles/match.scss"
 const opendota_api = "https://api.opendota.com"
 
 class MatchDetail extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    getHeroIcon(hero) {
+        if (hero != null && hero.hasOwnProperty("icon")) {
+            return `${opendota_api}${hero.icon}`
+        }
+
+        return ""
+    }
+
+    getHeroName(hero) {
+        if (hero != null && hero.hasOwnProperty("localized_name")) {
+            return hero.localized_name
+        }
+
+        return ""
+    }
+
     render () {
         return (
             <div className="match-detail">
@@ -21,13 +41,13 @@ class MatchDetail extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.matchDetails.players && this.props.matchDetails.players.map(function(player, index) {
+                        {this.props.matchDetails.players && this.props.matchDetails.players.map((player, index) => {
                             return (
                                 <tr key={index}>
                                     <td>
-                                        <img className='icon-small' src={`${opendota_api}${heroes[player.hero_id].icon}`} />
+                                        <img className="icon-small" src={this.getHeroIcon(heroes[player.hero_id])} />
                                     </td>
-                                    <td>{heroes[player.hero_id].localized_name} ({player.account_id})</td>
+                                    <td>{this.getHeroName(heroes[player.hero_id])} ({player.account_id})</td>
                                     <td>{player.kills}</td>
                                     <td>{player.deaths}</td>
                                     <td>{player.assists}</td>
